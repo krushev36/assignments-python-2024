195760d3838e87e534d73758f9e4e8e040634fbe
sergio sanes negrete <sergio.sanes@gmail.com>
2024-08-21 02:11:36 +0000

    hola

--- assingment/main.py
+++ assingment/main.py
@@ -1,6 +1,13 @@
-def main():
-    ...
+import pandas as pd
 
+
+def main():
+    #df = pd.read_csv("data.txt", sep="\t", header=None)
+    #print(df)
+    
+    with(open("data.csv", "r")) as file:
+        data = file.readlines()
+    print(data)
 
 if __name__ == "__main__":
     main()
